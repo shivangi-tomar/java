537208cb91abcce1fe3b674febeb48c2ad6831d7
Shivangi Tomar <144542760+shivangi-tomar@users.noreply.github.com>
2024-08-23 08:58:33 +0530

    Time: 4 ms (100%), Space: 44.6 MB (5.67%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+       if(x<0||(x%10==0&&x!=0)) {
+        return false;
+       }
+       int reversednum=0;
+       while(x>reversednum){
+        reversednum=reversednum*10+x%10;
+        x/=10;
+       }
+       return x==reversednum||x==reversednum/10;
+    }
+}
