46a724e756efbb81e9168c343d36db69de489c1e
Shivangi Tomar <144542760+shivangi-tomar@users.noreply.github.com>
2025-01-14 19:19:21 +0530

    Time: 0 ms (100%), Space: 45.2 MB (23.1%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -4,16 +4,23 @@ class Solution {
         int right = nums.length - 1;
 
         while (left < right) {
-            // Agar left index pe even number hai to aage badho
+
+
+
+            // Agar left index pe even number h to aage jana h
             if (nums[left] % 2 == 0) {
                 left++;
             } 
-            // Agar right index pe odd number hai to peeche jao
+
+            // Agar right index pe odd number h to peeche jana h
             else if (nums[right] % 2 != 0) {
                 right--;
             } 
-            // Agar left pe odd aur right pe even hai to swap kar do
+
+            // Agar left pe odd aur right pe even hai to swap karna
+
             else {
+                
                 int temp = nums[left];
                 nums[left] = nums[right];
                 nums[right] = temp;
