349963f3b3ded561c6a134c10a1bcef428a8a6ac
Shivangi Tomar <144542760+shivangi-tomar@users.noreply.github.com>
2025-01-17 13:20:18 +0530

    Time: 0 ms (100%), Space: 42 MB (14.92%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -13,4 +13,18 @@
  *     }
  * }
  */
-class Solution { public List<Integer> postorderTraversal(TreeNode root) { List<Integer> result = new ArrayList<>(); postorder(root, result); return result; } private void postorder(TreeNode node, List<Integer> result) { if (node != null) { postorder(node.left, result); postorder(node.right, result); result.add(node.val); } } }
+class Solution { 
+    public List<Integer> postorderTraversal(TreeNode root) { 
+        List<Integer> result = new ArrayList<>(); 
+        postorder(root, result); 
+        return result; 
+        } 
+        private void postorder(TreeNode node, List<Integer> result) 
+        { 
+            if (node != null) { 
+                postorder(node.left, result); 
+                postorder(node.right, result); 
+                result.add(node.val); 
+            } 
+        } 
+    }
