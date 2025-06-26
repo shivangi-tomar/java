11664c2d1a20a89be61d8b72210945dbc81556e6
Shivangi Tomar <144542760+shivangi-tomar@users.noreply.github.com>
2025-06-26 12:53:09 +0530

    Time: 0 ms (100%), Space: 40.3 MB (76.63%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.java
+++ 0062-unique-paths/0062-unique-paths.java
@@ -1,9 +1,15 @@
-public class Solution {
+class Solution {
     public int uniquePaths(int m, int n) {
-        long ans = 1;
-        for (int i = 1; i <= m - 1; i++) {
-            ans = ans * (n - 1 + i) / i;
+        int N = m + n - 2;
+        int r = Math.min(m - 1, n - 1); // Use the smaller of the two to optimize
+        long result = 1;
+
+        for (int i = 1; i <= r; i++) {
+            result = result * (N - r + i) / i;
         }
-        return (int)ans;
+
+        return (int) result;
     }
 }
+
+
